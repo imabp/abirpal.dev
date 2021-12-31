b6fb16f63b3311cd85396b3dae8539fffe17d5da
Abir <abir.p@affinidi.com>
2021-12-31 19:01:56 +0530

    ⚙️ Update Layout based on About Page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,33 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
+import { home } from "../routes.config";
 interface LayoutProps {
   children: React.ReactNode;
   title?: string;
   slug?: string;
   bgPattern?: boolean;
   footer?: boolean | undefined;
+  aboutpage?: boolean;
 }
 
-const Layout = ({ children, title, slug, bgPattern, footer }: LayoutProps) => {
+const Layout = ({
+  children,
+  title,
+  slug,
+  bgPattern,
+  footer,
+  aboutpage = false,
+}: LayoutProps) => {
   if (footer === undefined) {
     footer = true;
   }
   return (
     <div
-      className={`w-screen h-screen m-0 p-0
+      h-full
+      className={`w-screen h-full m-0 p-0 bg-secondary 
     ${bgPattern ? "bg-layout-pattern bg-repeat-x	 bg-bottom" : ""} 
     `}
     >
@@ -28,20 +39,60 @@ const Layout = ({ children, title, slug, bgPattern, footer }: LayoutProps) => {
       <>
         <div
           id="header"
-          className={` mt-8 mb-5 
+          className={` pt-8 mb-5 
           iphones:ml-5 iphonex:ml-10
           iphones:mr-5 iphonex:mr-10
           ipad:ml-15 ipadpro:ml-15 desktop:ml-15
           ipad:mr-15 ipadpro:mr-15 desktop:mr-15
       `}
         >
-          <p
-            className="iphones:text-fs44  iphonex:text-fs50 
+          {aboutpage && (
+            <>
+              {" "}
+              <p
+                className="
+            text-center
+            iphones:text-fs44  iphonex:text-fs50 
            ipadpro:text-fs50 ipad:text-fs50
            desktop:text-fs50"
-          >
-            {title ? title : <p className="invisible">invisible_title</p>}
-          </p>
+              >
+                {title}
+              </p>
+              <p className="text-center ">
+                <Link href={home.route} passHref>
+                  <Image
+                    className="cursor-pointer"
+                    src="/system/vectors/home.svg"
+                    layout="fixed"
+                    height="24"
+                    width="24"
+                  />
+                </Link>
+              </p>
+            </>
+          )}
+
+          {!aboutpage && (
+            <p
+              className="iphones:text-fs44  iphonex:text-fs50 
+           ipadpro:text-fs50 ipad:text-fs50
+           desktop:text-fs50"
+            >
+              {title ? title : <p className="invisible">invisible_title</p>}
+              {title && (
+                <span className="iphones:text-fs24 cursor-pointer ml-5">
+                  <Link href={home.route} passHref>
+                    <Image
+                      src="/system/vectors/home.svg"
+                      layout="fixed"
+                      height="24"
+                      width="24"
+                    />
+                  </Link>
+                </span>
+              )}
+            </p>
+          )}
 
           <p
             className="iphones:text-fs18 iphonex:text-fs30 
@@ -50,7 +101,7 @@ const Layout = ({ children, title, slug, bgPattern, footer }: LayoutProps) => {
           >
             {slug ? slug : <p className="invisible">invisible_slug</p>}
           </p>
-          {title && (
+          {slug && (
             <Image
               src="/system/vectors/line.svg"
               layout="responsive"
